4b375a79bd56481a61f0fb99683819f412fbe042
RIKKA421 <221300061@samil.nju.edu.cn>
2024-09-13 20:32:59 +0800

    Site updated: 2024-09-13 20:32:57

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1726222750389";
+const VERSION = "1726230748924";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
