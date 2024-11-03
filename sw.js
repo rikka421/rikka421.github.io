e6643ce89d5d9806334fb22bd5a7bbeeb3d48ba0
rikka421 <221300061@smail.nju.edu.cn>
2024-11-03 23:41:23 +0800

    Site updated: 2024-11-03 23:41:22

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1730647415878";
+const VERSION = "1730648480160";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
